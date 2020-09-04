399174c7c42ba8c6980c5f41132797da53352fa8
Lanrna Sayasean <lanrna@bigsouthernsoftware.com>
2020-09-04 11:13:29 -0400

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,6 +10,21 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+import 'bootstrap';
+import '../../assets/stylesheets/application.scss';
+import Rails from '@rails/ujs';
+
+window.jQuery = $;
+window.$ = $;
+
+Rails.start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+
+// prevent double renders by destroying js widgets
+// before cache
+document.addEventListener('turbolinks:before-cache', () => {
+  $('select').each((n, ele) => {
+    if (ele.selectize) ele.selectize.destroy();
+  });
+});
