0454f4e1200147c2ea2b4c9b6a90e4d70852c1cf
Lanrna Sayasean <lanrna@bigsouthernsoftware.com>
2020-09-08 13:52:57 -0400

    created a posting area

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root  'posts#index'
   devise_for :users
-  root  'users#index'
+  resources :posts
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_011524) do
+ActiveRecord::Schema.define(version: 2020_09_04_192215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
